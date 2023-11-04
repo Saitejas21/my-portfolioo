80a4959c7a39af601ab349db988ec226c9c11800
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2023-11-04 12:40:46 +0530

    coming soon page

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,21 @@
+import Image from "next/image";
+
+
+
 export default function Home() {
   return (
     <>
-      <div className="container mx-auto px-4">
-        <h1>Hello word</h1>
-        <p>This is a content to make our page longer</p>
-        <div className="w-full h-screen bg-green-300"></div>
-        <p>
-          Lorem Ipsum is simply dummy text ...
-        </p>
-      </div>
+      <div class="wrapper">
+  <h1>coming soon<span class="dot">.</span></h1>
+  <p>Looks like ur too early over here,<br/>
+    Sai Tejas is still busy constructing this site.<br/> Come back soon to see many updates</p>
+  {/* <div class="icons">
+    <a href="https://behance.net/saitejasar"><i class="fa fa-behance"></i></a>
+    <a href="https://instagram.com/saitejas.buidl"><i class="fa fa-instagram"></i></a>
+    <a href="https://saitejasar.netlify.app"><i class="fa fa-user"></i></a>
+  </div> */}
+ </div>
+      
     </>
   );
 }
